6b41c78184964ea2ff03d86397f56338845456a9
Andrey Nikel <nikelandrey94@gmail.com>
2023-10-05 10:23:21 +0600

    assert tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,13 +1,6 @@
 from main_package.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно! пустая строка')
-
-if capitalize('2') != '2':
-    raise Exception('Функция работает неверно! c 1 символом')
-
-if capitalize(' ') != ' ':
-    raise Exception('Функция работает неверно! c пробелом')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
+assert capitalize('2') == '2'
+assert capitalize(' ') == ' '
